400234634ef587c5ca6fd78e4f02730b2f531040
Nathan <nathan.lima@mkdata.com.br>
2025-06-13 17:03:19 -0300

    Ajustes para chamar a API Spring Boot e retornar na aplicação

--- crud-angular/src/app/courses/services/courses.ts
+++ crud-angular/src/app/courses/services/courses.ts
@@ -8,7 +8,7 @@ import { Course } from '../models/course';
 })
 export class CoursesService {
 
-  private readonly API = '/assets/courses.json'
+  private readonly API = 'api/courses'
 
  constructor(private readonly http : HttpClient) {}
 
