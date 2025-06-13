950ff8e4edfaf43a248b3fa14b0f8eff4682f46c
Nathan <nathan.lima@mkdata.com.br>
2025-06-13 17:02:06 -0300

    Alteração do retorno do id para _id

--- crud-spring/src/main/java/com/nathan/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/nathan/crud_spring/controller/CourseController.java
@@ -2,7 +2,6 @@ package com.nathan.crud_spring.controller;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- crud-spring/src/main/java/com/nathan/crud_spring/model/Course.java
+++ crud-spring/src/main/java/com/nathan/crud_spring/model/Course.java
@@ -1,5 +1,7 @@
 package com.nathan.crud_spring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,6 +15,7 @@ public class Course {
     
     @Id
     @GeneratedValue( strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
 
     @Column(name = "nome", length = 250, nullable = false)
