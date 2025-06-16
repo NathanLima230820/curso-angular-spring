ec7232ef2a11b2ec4627724ae9d97b577803411e
Nathan <nathan.lima@mkdata.com.br>
2025-06-16 17:27:10 -0300

    Criação do método POST retornando 201

--- crud-spring/src/main/java/com/nathan/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/nathan/crud_spring/controller/CourseController.java
@@ -2,14 +2,21 @@ package com.nathan.crud_spring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nathan.crud_spring.model.Course;
 import com.nathan.crud_spring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/api/courses")
@@ -22,5 +29,12 @@ public class CourseController {
     public List<Course> list() {
         return courseRepository.findAll();
     }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course insertOrUpdate(@RequestBody Course course){
+        return courseRepository.save(course);
+    }
+    
 
 }
