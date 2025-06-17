39d51b7de92e59b83c2405da92c8eed0021dc0c0
Nathan <nathan.lima@mkdata.com.br>
2025-06-17 17:30:02 -0300

    Refatoração de objetos

--- crud-spring/src/main/java/com/nathan/crud_spring/controller/CourseController.java
+++ crud-spring/src/main/java/com/nathan/crud_spring/controller/CourseController.java
@@ -3,8 +3,6 @@ package com.nathan.crud_spring.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
