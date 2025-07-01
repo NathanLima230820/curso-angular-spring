dfddfb1cc032fb6d61106253b5cacd284a72ba3b
Nathan <nathan.lima@mkdata.com.br>
2025-07-01 17:27:51 -0300

    Criação do Dialog de confirmação para exclusão

--- crud-angular/src/app/courses/containers/courses/courses.ts
+++ crud-angular/src/app/courses/containers/courses/courses.ts
@@ -10,6 +10,7 @@ import { CoursesService } from '../../services/courses.service';
 import { ErrorDialog } from '../../../shared/components/error-dialog/error-dialog';
 import { CoursesGrid } from "../../components/courses-grid/courses-grid";
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ConfirmationDialog } from '../../../shared/components/confirmation-dialog/confirmation-dialog';
 
 @Component({
   selector: 'app-courses',
@@ -66,7 +67,14 @@ export class Courses implements OnInit{
   }
 
   onDelete(courses: Course){
-    this.CoursesService.delete(courses._id).subscribe(
+
+    const dialogRef = this.dialog.open(ConfirmationDialog, {
+      data: 'Tem certeza que deseja remover esse curso?',
+    });
+
+    dialogRef.afterClosed().subscribe((result:boolean) => {
+      if (result) {
+        this.CoursesService.delete(courses._id).subscribe(
       () => {
         this.refresh();
         this.snackBar.open('Curso removido com sucesso!', 'X',{
@@ -77,6 +85,8 @@ export class Courses implements OnInit{
       },
       () => this.onError('Erro ao tentar remover curso.')
     );
+      }
+    });
   }
 
 }
--- crud-angular/src/app/shared/components/confirmation-dialog/confirmation-dialog.ts
+++ crud-angular/src/app/shared/components/confirmation-dialog/confirmation-dialog.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { AppMaterialModule } from '../../app-material/app-material-module';
 
 @Component({
   selector: 'app-confirmation-dialog',
   imports: [
     MatDialogModule,
-
+    AppMaterialModule
   ],
   templateUrl: './confirmation-dialog.html',
   styleUrl: './confirmation-dialog.scss'
 })
 
 export class ConfirmationDialog {
+
+  readonly dialogRef = inject(MatDialogRef<ConfirmationDialog>);
+  readonly data = inject<string>(MAT_DIALOG_DATA);
+
+  onConfirm(result: boolean): void {
+    this.dialogRef.close(result);
+  }
 
 }
