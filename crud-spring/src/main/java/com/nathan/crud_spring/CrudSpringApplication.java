4f75e971baa42d6832c08db75998dda89a8fca41
Nathan <nathan.lima@mkdata.com.br>
2025-07-16 08:33:52 -0300

    Alteração no initDataBase apenas uma modificação no 'if'

--- crud-spring/src/main/java/com/nathan/crud_spring/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/nathan/crud_spring/CrudSpringApplication.java
@@ -29,7 +29,7 @@ public class CrudSpringApplication {
 		return args -> {
 			courseRepository.deleteAll();
 
-			for (int i = 0; i <= 20; i++) {
+			for (int i = 0; i < 20; i++) {
 
 
 			Course c = new Course();
